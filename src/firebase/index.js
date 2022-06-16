ee0d2011146b4b5f451e343cb9cb7a83c21d2fc5
chriiss <christopher.kumla@gmail.com>
2022-06-16 16:15:20 +0200

    update firebase file

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHI7U1eNaUuA4ZuhgV_9edGH8OdXtQogo",
-  authDomain: "vue-firebase-auth-ece24.firebaseapp.com",
-  projectId: "vue-firebase-auth-ece24",
-  storageBucket: "vue-firebase-auth-ece24.appspot.com",
-  messagingSenderId: "736918306509",
-  appId: "1:736918306509:web:e674d4ae3689535061f384"
+  apiKey: "****************",
+  authDomain: "***",
+  projectId: "*****",
+  storageBucket: "*******",
+  messagingSenderId: "*****",
+  appId: "*****"
 };
 
 const app = initializeApp(firebaseConfig);
